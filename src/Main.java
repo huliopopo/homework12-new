ec260a5c76d05fea3f2e51f4361745be87490310
huliopopo <yashin.hlpp@gmail.com>
2024-03-17 11:27:35 +0300

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        Author nikolaiGogol = new Author("Николай", "Гоголь");
-        Author alexDuma = new Author("Александр", "Дюма");
-        Book revizor = new Book("Ревизор", nikolaiGogol, 1836);
-        Book graphMonteKristo = new Book("Граф Монте-Кристо", alexDuma, 1844);
-        System.out.println("revizor.getTitle() = " + revizor.getTitle());
-        System.out.println("revizor.getAuthor() = " + revizor.getAuthor());
-        System.out.println("revizor.getYear() = " + revizor.getYear());
+        Author author1 = new Author("Николай", "Гоголь");
+        Author author2 = new Author("Александр", "Дюма");
+        Book book1 = new Book("Ревизор", author1, 1836);
+        Book book2 = new Book("Граф Монте-Кристо", author2, 1844);
+        Book book3 = new Book("Ревизор", author1, 8888);
+        System.out.println("revizor.getTitle() = " + book1.getTitle());
+        System.out.println("revizor.getAuthor() = " + book1.getAuthor());
+        System.out.println("revizor.getYear() = " + book1.getYear());
 
-        revizor.setYear(1888);
-        System.out.println("revizor.getYear() = " + revizor.getYear());
+        book1.setYear(8888);
+        System.out.println("revizor.getYear() = " + book1.getYear());
+
+        System.out.println(book1);
+
+        if (book1.equals(book3)) {
+            System.out.println("Книга такая же");
+        } else {
+            System.out.println("Книги разные");
+        }
+
+        System.out.println(book1.hashCode());
+        System.out.println(book2.hashCode());
+        System.out.println(book3.hashCode());
+
+        if (author1.equals(author2)) {
+            System.out.println("Автот тот же");
+        } else System.out.println("Авторы разные");
+
+        }
 
     }
-}
